c0525f2e39ab78ac1b3c62173ba7b5c773f11b20
明国宾 <mingguobin@live.com>
2019-12-31 21:55:00 +0800

    init: init the whole project

---
+++ src/main/java/org/daming/hoteler/HotelerApplication.java
@@ -0,0 +1,13 @@
+package org.daming.hoteler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/daming/hoteler/HotelerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.daming.hoteler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
