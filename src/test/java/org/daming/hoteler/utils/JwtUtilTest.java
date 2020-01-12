1cb0b0f69cbbb549dc77b9c2a7d2d263c0859105
damingeridai <mingguobin@live.com>
2020-01-12 18:51:09 +0800

    test: add unit test for jwt util

---
+++ src/test/java/org/daming/hoteler/utils/JwtUtilTest.java
@@ -0,0 +1,57 @@
+package org.daming.hoteler.utils;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JwtUtilTest {
+
+    @Test
+    void createJWT() {
+        var k = "daming";
+        var key = JwtUtil.generalKey(k);
+        var id = "daming";
+        var subject = "damingerdai";
+        var ttlMillis = 60 * 1000L;
+        var map = new HashMap<String, Object>(4);
+        map.put("username", "daming");
+        var token = JwtUtil.createJWT(id, subject, ttlMillis, key, map);
+        assertNotNull(token);
+    }
+
+    @Test
+    void parseJwt() {
+        var k = "daming";
+        var key = JwtUtil.generalKey(k);
+        var id = "daming";
+        var subject = "damingerdai";
+        var ttlMillis = 60 * 1000L;
+        var map = new HashMap<String, Object>(4);
+        map.put("username", "daming");
+        var token = JwtUtil.createJWT(id, subject, ttlMillis, key, map);
+        var claims = JwtUtil.parseJwt(token, key);
+        assertEquals(id, claims.getId());
+        assertEquals(subject, claims.getSubject());
+        assertEquals("daming", claims.get("username"));
+        try {
+            TimeUnit.SECONDS.sleep(65L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Assertions.assertThrows(ExpiredJwtException.class, () -> {
+            JwtUtil.parseJwt(token, key);
+        });
+    }
+
+    @Test
+    void generalKey() {
+        var k = "daming";
+        var key = JwtUtil.generalKey(k);
+        assertNotNull(key);
+    }
+}
