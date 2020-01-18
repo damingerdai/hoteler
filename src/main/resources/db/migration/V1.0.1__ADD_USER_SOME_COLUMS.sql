bb67cdaaee25eec83817c21c8f52021821acf6d6
damingeridai <mingguobin@live.com>
2020-01-18 20:07:47 +0800

    feat(db:flyway): update table  user for create/update user/date

---
+++ src/main/resources/db/migration/V1.0.1__ADD_USER_SOME_COLUMS.sql
@@ -0,0 +1,4 @@
+ALTER TABLE users ADD create_dt timestamp;
+ALTER TABLE users ADD create_user varchar(50);
+ALTER TABLE users ADD update_dt timestamp;
+ALTER TABLE users ADD update_user varchar(50);
