5457f8c3f9e189a86ec52968af325a23db5255bf
damingeridai <mingguobin@live.com>
2020-01-19 13:26:01 +0800

    feat(service:user): support get user info by user name

--- src/main/java/org/daming/hoteler/service/IUserService.java
+++ src/main/java/org/daming/hoteler/service/IUserService.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface IUserService {
 
     List<User> list();
+
+    User getUserByUsername(String username);
 }
--- src/main/java/org/daming/hoteler/service/impl/UserServiceImpl.java
+++ src/main/java/org/daming/hoteler/service/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package org.daming.hoteler.service.impl;
 
+import org.daming.hoteler.dao.jdbc.IUserDao;
 import org.daming.hoteler.dao.mapper.UserMapper;
 import org.daming.hoteler.pojo.User;
 import org.daming.hoteler.service.IUserService;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import java.util.List;
 
@@ -12,12 +14,20 @@ public class UserServiceImpl implements IUserService {
 
     private UserMapper userMapper;
 
+    private IUserDao userDao;
+
     @Override
     public List<User> list() {
         return this.userMapper.list();
     }
 
-    public UserServiceImpl(UserMapper userMapper) {
+    @Override
+    public User getUserByUsername(String username) {
+        Assert.hasText("username", "params 'username' is required");
+        return userDao.getUserByUsername(username).get();
+    }
+
+    public UserServiceImpl(UserMapper userMapper,  IUserDao userDao) {
         super();
         this.userMapper = userMapper;
     }
