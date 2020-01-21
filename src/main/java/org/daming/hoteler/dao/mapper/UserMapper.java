dc5ea34a4d293f14a6ce66374c789b20e586f183
damingeridai <mingguobin@live.com>
2020-01-21 21:41:59 +0800

    feat(user): add create user method

--- src/main/java/org/daming/hoteler/dao/mapper/UserMapper.java
+++ src/main/java/org/daming/hoteler/dao/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package org.daming.hoteler.dao.mapper;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -19,4 +20,7 @@ public interface UserMapper {
 
     @Update("insert into users (id, username, password, create_dt, create_user, update_dt, update_user) values (#{id}, #{username}, #{password}, now(), 'UserMapper', now(), 'UserMapper') ")
     void create(User user);
+
+    @Insert("insert into users (id, username, password, create_dt, create_user, update_dt, update_user) values (#{id}, #{username}, #{password}, statement_timestamp(),'system', statement_timestamp(), 'system')")
+    void create(@Param("id")long id, @Param("username")String username, @Param("password")String password);
 }
