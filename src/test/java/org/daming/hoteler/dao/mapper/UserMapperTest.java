4117e877ac17f772a7ef82842b4a4222a3e7b9d7
damingeridai <mingguobin@live.com>
2020-02-04 20:48:44 +0800

    test: support transaction in unit test

--- src/test/java/org/daming/hoteler/dao/mapper/UserMapperTest.java
+++ src/test/java/org/daming/hoteler/dao/mapper/UserMapperTest.java
@@ -3,14 +3,20 @@ package org.daming.hoteler.dao.mapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
+@Rollback(true)
 class UserMapperTest {
 
     @Autowired(required = false)
     private UserMapper userMapper;
+
+
 
     @Test
     void list() {
