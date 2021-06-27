3edb3683b6c584360b82fc1e23870a83530f4433
damingerdai <mingguobin@live.com>
2021-06-27 10:57:46 +0800

    test: 修复ICustomerStatServiceTest不通过的错误

--- src/test/java/org/daming/hoteler/service/stat/ICustomerStatServiceTest.java
+++ src/test/java/org/daming/hoteler/service/stat/ICustomerStatServiceTest.java
@@ -1,10 +1,16 @@
 package org.daming.hoteler.service.stat;
 
+import org.daming.hoteler.repository.mapper.UserRoomMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class ICustomerStatServiceTest {
@@ -12,9 +18,14 @@ class ICustomerStatServiceTest {
     @Autowired
     private ICustomerStatService customerStatService;
 
+    @MockBean
+    private UserRoomMapper userRoomMapper;
+
     @Test
     public void countPastWeekCustomerCountStatTest() {
+        when(userRoomMapper.getUserRoomCounts(any(LocalDateTime.class), any(LocalDateTime.class))).thenReturn(1);
         this.customerStatService.countPastWeekCustomerCountStat();
+        verify(userRoomMapper, times(7)).getUserRoomCounts(any(LocalDateTime.class), any(LocalDateTime.class));
     }
 
 }
