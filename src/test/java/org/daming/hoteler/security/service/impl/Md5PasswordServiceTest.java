605710bb6847350816a347a381863c2620c4b11f
damingerdai <mingguobin@live.com>
2021-07-17 15:45:26 +0800

    test: 补充Md5PasswordService解密密码单元测试

--- src/test/java/org/daming/hoteler/security/service/impl/Md5PasswordServiceTest.java
+++ src/test/java/org/daming/hoteler/security/service/impl/Md5PasswordServiceTest.java
@@ -1,5 +1,6 @@
 package org.daming.hoteler.security.service.impl;
 
+import org.daming.hoteler.base.exceptions.HotelerException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -25,6 +26,13 @@ class Md5PasswordServiceTest {
         var password = "123456";
         var encodePassword = md5PasswordService.encodePassword(password);
         assertEquals("e10adc3949ba59abbe56e057f20f883e", encodePassword);
+    }
+
+    @Test
+    @DisplayName("Md5PasswordService解密密码单元测试")
+    public void testDecodePassword() {
+        var thrown  = assertThrows(HotelerException.class, () -> md5PasswordService.decodePassword("e10adc3949ba59abbe56e057f20f883e"));
+        assertEquals(thrown.getMessage(), "md5 algorithm encrypt is not supported");
     }
 
 }
