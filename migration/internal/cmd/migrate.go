0f8f5072e4fa75df8a275f71a3d764026ddbb2d8
Arthur Ming <guobin.ming@pwc.com>
2021-08-26 20:36:44 +0800

    feat(migration): pg数据库连接的sslmode将由'POSTGRES_SSLMODE'控制

--- migration/internal/cmd/migrate.go
+++ migration/internal/cmd/migrate.go
@@ -10,13 +10,17 @@ import (
 
 func FetchMigrate() (*migrate.Migrate, error) {
 	dbUrl := fmt.Sprintf(
-		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		"postgres://%s:%s@%s:%s/%s",
 		os.Getenv("POSTGRES_USER"),
 		url.QueryEscape(os.Getenv("POSTGRES_PASSWORD")),
 		os.Getenv("POSTGRES_HOST"),
 		os.Getenv("POSTGRES_PORT"),
 		os.Getenv("POSTGRES_DB"),
 	)
+	sslmode := os.Getenv("POSTGRES_SSLMODE")
+	if sslmode == "" {
+		dbUrl += "?sslmode=" + sslmode
+	}
 	return migrate.New(
 		"file://./db/migrations",
 		dbUrl,
