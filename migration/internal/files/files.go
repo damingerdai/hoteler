0d7de40926471fc4878ece2dab088152996ea138
Arthur Ming <guobin.ming@pwc.com>
2021-09-08 09:10:13 +0800

    fix: the words beginning with capital letters will not be added '-' in Kebabcase function

--- migration/internal/files/files.go
+++ migration/internal/files/files.go
@@ -2,9 +2,13 @@ package files
 
 func Kebabcase(s string) string {
 	sl := make([]rune, 0, len(s)*2)
-	for _, v := range s {
+	for i, v := range s {
 		if v >= 65 && v <= 90 {
-			sl = append(sl, 95, v+32)
+			if i == 0 {
+				sl = append(sl, v+32)
+			} else {
+				sl = append(sl, 95, v+32)
+			}
 		} else {
 			sl = append(sl, v)
 		}
