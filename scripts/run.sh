64b66e102bf281ab48a787bcc746f52fd58d9eba
Arthur Ming <guobin.ming@pwc.com>
2021-11-27 23:01:57 +0800

    build: 添加spring boot的部署脚本

---
+++ scripts/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo stop application
+source ./stop.sh
+echo start application
+source ./start.sh
+exit 0
---
+++ scripts/start.sh
@@ -0,0 +1,1 @@
+nohup java -jar -Xmx384m -XX:MaxMetaspaceSize=128m -XX:CompressedClassSpaceSize=24m hoteler-standalone.jar --spring.config.location=./application.yml  &
