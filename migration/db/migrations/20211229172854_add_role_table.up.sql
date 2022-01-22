96fe33c4d515b3fe9ff912c97500615ec4399215
damingerdai <mingguobin@live.com>
2022-01-22 22:15:35 +0800

    fix(migration): 修复add_role_table的分号错误语法

--- migration/db/migrations/20211229172854_add_role_table.up.sql
+++ migration/db/migrations/20211229172854_add_role_table.up.sql
@@ -6,7 +6,7 @@ CREATE TABLE roles (
     create_user varchar(50) not null,
     update_dt timestamp not null,
     update_user varchar(50) not null
-)
+);
 
 INSERT INTO roles (id, name, description, create_dt, create_user, update_dt, update_user) VALUES(1, 'admin', 'Admin', now(), 'system', now(), 'system');
 INSERT INTO roles (id, name, description, create_dt, create_user, update_dt, update_user) VALUES(2, 'users', 'Standard User', now(), 'system', now(), 'system');
