6f05f87059628422c928f3817a00f963a38ba67f
Arthur Ming <guobin.ming@pwc.com>
2022-05-27 18:07:47 +0800

    style: remmove useless code

--- src/main/java/org/daming/hoteler/config/WebSecurityConfig.java
+++ src/main/java/org/daming/hoteler/config/WebSecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
