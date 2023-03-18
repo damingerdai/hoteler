60edbc8cae205246f86e307e2ab4bf8fdeaadc0a
damingerdai <mingguobin@live.com>
2023-03-18 09:27:53 +0800

    feat: 使用Jackson2JsonRedisSerialize 替换默认序列化(默认采用的是JDK序列化)

--- src/main/java/org/daming/hoteler/config/RedisConfig.java
+++ src/main/java/org/daming/hoteler/config/RedisConfig.java
@@ -1,5 +1,8 @@
 package org.daming.hoteler.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.daming.hoteler.base.support.ByteArrayRedisSerializer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.cache.CacheManager;
@@ -12,6 +15,7 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -61,6 +65,10 @@ public class RedisConfig {
     }
 
     private RedisSerializer<Object> valueSerializer() {
-        return new ByteArrayRedisSerializer();
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.activateDefaultTyping(om.getPolymorphicTypeValidator(), ObjectMapper.DefaultTyping.NON_FINAL);
+        return jackson2JsonRedisSerializer;
     }
 }
