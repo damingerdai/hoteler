a37ecb4fbc2f4bdfe1d8ecec0ff785d432555b07
Arthur Ming <guobin.ming@pwc.com>
2023-08-26 19:07:37 +0800

    feat: 添加获取本地ip的工具类

---
+++ src/main/java/org/daming/hoteler/utils/IpUtil.java
@@ -0,0 +1,51 @@
+package org.daming.hoteler.utils;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+
+/**
+ * link: <a href="https://juejin.cn/post/7015586556758523918">Java实现获取本机Ip工具类</a>
+ * @author gming001
+ * @version 2023-08-26 19:04
+ */
+public class IpUtil {
+
+    public static final String DEFAULT_IP = "127.0.0.1";
+
+    /**
+     * 直接根据第一个网卡地址作为其内网ipv4地址，避免返回 127.0.0.1
+     *
+     * @return
+     */
+    public static String getLocalIpByNetcard() {
+        try {
+            for (Enumeration<NetworkInterface> e = NetworkInterface.getNetworkInterfaces(); e.hasMoreElements(); ) {
+                NetworkInterface item = e.nextElement();
+                for (InterfaceAddress address : item.getInterfaceAddresses()) {
+                    if (item.isLoopback() || !item.isUp()) {
+                        continue;
+                    }
+                    if (address.getAddress() instanceof Inet4Address inet4Address) {
+                        return inet4Address.getHostAddress();
+                    }
+                }
+            }
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (SocketException | UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String getLocalIP() {
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
