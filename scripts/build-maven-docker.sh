8df2f984c5d207b3578d4b786fc39a87219d84bb
Arthur Ming <guobin.ming@pwc.com>
2024-07-22 14:17:10 +0800

    build: 更新build docker脚本

--- scripts/build-maven-docker.sh
+++ scripts/build-maven-docker.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Get the current commit SHA
 sha=$(git rev-parse HEAD)
 
-docker build -t hoteler-maven-api:${sha} -f maven.Dockerfile .
+# Define the Docker image name
+image_name="hoteler-api"
+
+# Build the Docker image with the current commit SHA as the tag
+docker build -t ${image_name}:${sha} -f maven.Dockerfile .
+
+# Print a success message
+echo "Docker image ${image_name}:${sha} built successfully."
+
