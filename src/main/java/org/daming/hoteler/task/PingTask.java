71873ab744557c06995ca7ccf3e2edc7112d11ec
damingerdai <mingguobin@live.com>
2025-01-01 18:40:29 +0800

    refactor: 使用ping task定期访问redis和pg的状态

---
+++ src/main/java/org/daming/hoteler/task/PingTask.java
@@ -0,0 +1,31 @@
+package org.daming.hoteler.task;
+
+import org.daming.hoteler.base.logger.HotelerLogger;
+import org.daming.hoteler.base.logger.LoggerManager;
+import org.daming.hoteler.service.IPingService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PingTask {
+
+    private final HotelerLogger logger = LoggerManager.getJobLogger();
+
+    private final IPingService pingService;
+
+    @Scheduled(fixedRate = 5000)
+    public void runPing() {
+        try {
+            this.logger.info("run ping redis and db task");
+            this.pingService.ping();
+            this.logger.info("complete ping redis and db task");
+        } catch (Exception ex) {
+            this.logger.error("fail to complete ping redis and db task: {}", ex.getMessage());
+        }
+
+    }
+
+    public PingTask(IPingService pingService) {
+        this.pingService = pingService;
+    }
+}
