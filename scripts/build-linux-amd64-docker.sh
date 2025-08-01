e5c815c7542f238de2ab9a5332133d0a4d230036
damingerdai <mingguobin@live.com>
2025-08-02 07:21:45 +0800

    chore: build linx amd64 docker image shell scripts

---
+++ scripts/build-linux-amd64-docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Get the current commit SHA
+sha=$(git rev-parse HEAD)
+
+# Define the Docker image name
+image_name="hoteler-api"
+
+# Build the Docker image with the current commit SHA as the tag
+docker build  --platform linux/amd64 -t ${image_name}:${sha} .
+
+# Print a success message
+echo "Docker image ${image_name}:${sha} built successfully."
+
