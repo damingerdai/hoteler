0cdc521c36fa62c41bd7a80c626fc42984080935
damingerdai <mingguobin@live.com>
2026-05-25 10:45:15 +0800

    chore: upgrade gradle 9.5.0

--- src/main/java/org/daming/hoteler/HotelerApplication.java
+++ src/main/java/org/daming/hoteler/HotelerApplication.java
@@ -10,7 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableCaching
 public class HotelerApplication {
 
-	static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(HotelerApplication.class, args);
 	}
 
